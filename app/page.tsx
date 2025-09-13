5222d589954e58cb64405e21d4c8444548421507
imriyarana <riyarana1437@gmail.com>
2025-09-13 15:47:53 +0530

    new updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 import React, { useState } from "react";
 
-import Header from "../sections/Header";
-import Home from "../sections/Home";
-import LogoTicker from "../sections/LogoTicker";
-import IceCream from "../sections/IceCream";
-import Boxes from "../sections/Boxes";
-import Reviews from "../sections/Reviews";
-import CallToAction from "../sections/CallToAction";
-import Contact from "../sections/Contact";
-import Footer from "../sections/Footer";
-import CartModal from "../sections/CartModal";
+import Header from "@/sections/Header";
+import Home from "@/sections/Home";
+import LogoTicker from "@/sections/LogoTicker";
+import IceCream from "@/sections/IceCream";
+import Boxes from "@/sections/Boxes";
+import Reviews from "@/sections/Reviews";
+import CallToAction from "@/sections/CallToAction";
+import Contact from "@/sections/Contact";
+import Footer from "@/sections/Footer";
+import CartModal from "@/sections/CartModal";
 
 const Page: React.FC = () => {
   const [isCartOpen, setIsCartOpen] = useState<boolean>(false);
